8abd0d5f9dbfedd8d8fb84435e64865f4bd9e308
Jelmer <luxizzle@gmail.com>
2017-04-15 17:58:45 +0200

    added watch

--- bin/pack.js
+++ bin/pack.js
@@ -4,12 +4,14 @@ var program = require('commander');
 var fs = require('fs');
 var path = require('path');
 var luamin = require('luamin');
+var moment = require('moment')
 
 var pack = require('../index.js')
 
 program
   .option('-i, --input <file>', 'File to pack')
   .option('-o, --output <name>', 'Output file name')
+  .option('-w, --watch', "Watch for file changes")
   .option('-m, --minify', "Minify packed output")
   .parse(process.argv)
 
@@ -18,16 +20,35 @@ if (!program.input) {
   process.exit(1);
 }
 
-program.output = program.output || ( path.basename(program.input, '.lua') + '_packed.lua' )
+if (!program.watch) {
 
-var packedContent = pack(program.input)
+  program.output = program.output || ( path.basename(program.input, '.lua') + '_packed.lua' )
 
-if (program.minify) {
-  packedContent = luamin.minify(packedContent);
+  var packedContent = pack(program.input)
+
+  if (program.minify) {
+    packedContent = luamin.minify(packedContent);
+  }
+
+  fs.writeFile(program.output, packedContent, function(err) {
+    if (err) throw err;
+
+    console.log('Packed file to ' + program.output)
+  })
+} else {
+  fs.watchFile(program.input, function() {
+    program.output = program.output || ( path.basename(program.input, '.lua') + '_packed.lua' )
+
+    var packedContent = pack(program.input)
+
+    if (program.minify) {
+      packedContent = luamin.minify(packedContent);
+    }
+
+    fs.writeFile(program.output, packedContent, function(err) {
+      if (err) throw err;
+
+      console.log('Packed file to ' + program.output + ' at ' + moment().format('HH:mm:ss'))
+    })
+  })
 }
-
-fs.writeFile(program.output, packedContent, function(err) {
-  if (err) throw err;
-
-  console.log('Packed file to ' + program.output)
-})
